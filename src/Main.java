7adce66ba57d61d073181dc5d55ff3ac28c6998e
sergeydomanov <nomad-labs@yandex.ru>
2022-08-15 21:48:59 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println();
+        System.out.println("Задание 1");
+
+        int[] arr = new int[] {2, 4, 8, 12, 22, 54, 91};
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++)
+            sum +=arr[i]; {
+            if (sum >= arr.length) {
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");}
+
+    }
     }
 }
+
+
+
+
+
