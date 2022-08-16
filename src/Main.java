9812ed0b41e8b20f4da251d3f4ca751bc00dff6c
sergeydomanov <nomad-labs@yandex.ru>
2022-08-16 22:05:04 +0300

    third exercise.

--- src/Main.java
+++ src/Main.java
@@ -69,7 +69,7 @@ public class Main {
 
         for (int i = reverseFullName.length-1;  i > 0;  i--);
         {
-            System.out.print(reverseFullName);
+            System.out.print(reverseFullName[i]);
         }
     }
 }
