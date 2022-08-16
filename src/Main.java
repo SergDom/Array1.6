dbde1b868d00168f3d98aa3ecce4dc7434fad2ec
sergeydomanov <nomad-labs@yandex.ru>
2022-08-16 22:02:10 +0300

    third exercise.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
 public class Main {
+
+        public static int[] generateRandomArray() {
+            java.util.Random random = new java.util.Random();
+            int[] arr = new int[30];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = random.nextInt(100_000) + 100_000;
+            }
+            return arr;
+        }
+
     public static void main(String[] args) {
+        int[] arr = generateRandomArray();
 
         System.out.println();
         System.out.println("Задание 1");
 
-        int[] arr = new int[]{2, 4, 8, 12, 22, 54, 91};
         int sum = 0;
 
         for (int i = 0; i < arr.length; i++)
