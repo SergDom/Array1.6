d0c6ca4ec57204db1df0ea19fd5933dcb3c3c032
sergeydomanov <nomad-labs@yandex.ru>
2022-08-18 21:49:24 +0300

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
 
-        public static int[] generateRandomArray() {
-            java.util.Random random = new java.util.Random();
-            int[] arr = new int[30];
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] = random.nextInt(100_000) + 100_000;
-            }
-            return arr;
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
         }
+        return arr;
+    }
 
     public static void main(String[] args) {
         int[] arr = generateRandomArray();
@@ -18,9 +18,11 @@ public class Main {
         int sum = 0;
 
         for (int i = 0; i < arr.length; i++) {
-            sum += arr[i];}
-            { System.out.println("Сумма трат за месяц составила " + sum + " рублей");
-            }
+            sum += arr[i];
+        }
+
+            System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+
 
 
         System.out.println();
@@ -30,39 +32,42 @@ public class Main {
         int minAmount = 300_000;
         for (int k = 0; k < arr.length; k++)
             if (arr[k] > maxAmount) {
-                maxAmount = arr[k];}
+                maxAmount = arr[k];
+            }
 
-        {
-        System.out.println("Максимальная сумма трат за день составила " + maxAmount + " рублей");}
+
+            System.out.println("Максимальная сумма трат за день составила " + maxAmount + " рублей");
+
 
         for (int j = 0; j < arr.length; j++)
             if (arr[j] < minAmount) {
                 minAmount = arr[j];
             }
-        {
-        System.out.println("Минимальная сумма трат за день составила " + minAmount + " рублей");}
+
+            System.out.println("Минимальная сумма трат за день составила " + minAmount + " рублей");
+
 
         System.out.println();
         System.out.println("Задание 3");
 
         double sumAver = 0;
 
-        for (int n = 0; n < arr.length; n++){
-            sumAver += arr[n];}
-            sumAver = sumAver / arr.length;
-        {
-            System.out.println("Средняя Сумма трат за месяц составила " + sumAver + " рублей");}
+        for (int n = 0; n < arr.length; n++) {
+            sumAver += arr[n]/arr.length;
+        }
+
+            System.out.println("Средняя Сумма трат за месяц составила " + sumAver + " рублей");
 
         System.out.println();
         System.out.println("Задание 4");
 
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
 
-        for (int i = reverseFullName.length-1;  i >= 0;  i--)
-        {
-            System.out.print(reverseFullName[i]);}
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
         }
     }
+}
 
 
 
